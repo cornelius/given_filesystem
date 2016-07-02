022b6c4ec9eb6ceee59b93a6c9b67493fe588ffd
Cornelius Schumacher <schumacher@kde.org>
2016-07-02 08:11:12 +0200

    Remove deprecation warning

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,8 @@
 require 'coveralls'
 Coveralls.wear!
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   Coveralls::SimpleCov::Formatter,
   SimpleCov::Formatter::HTMLFormatter
-]
+])
 
 require_relative('../lib/given_filesystem/spec_helpers')
