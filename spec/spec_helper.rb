1c0e4f2f2f15b57d8cb0c301a54cbd1f212a36e3
Cornelius Schumacher <schumacher@kde.org>
2017-02-22 19:20:53 +0100

    Don't use special coverage formatting
    The default formatting setup seems to work fine. Remove the custom
    configuration.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,5 @@
 require 'coveralls'
 Coveralls.wear!
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
-  Coveralls::SimpleCov::Formatter,
-  SimpleCov::Formatter::HTMLFormatter
-])
 
 require_relative('../lib/given_filesystem/spec_helpers')
 
